e6fadfa92ee108098d1723e179b24127bccc23e5
Otabek <60849894+OtabekSadiridinov@users.noreply.github.com>
2020-05-03 13:15:29 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MyGitAccounts/ishould.github.io.git master:gh-pages
+git push -f https://MyGitAccounts/.github.io/ishould.github.io.git master:gh-pages
 
 cd -
